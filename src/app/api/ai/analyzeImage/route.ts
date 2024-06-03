aa35599a17022181dbd9184c93758b63563855cc
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-03 17:35:48 -0500

    prisma installed

--- src/app/api/ai/analyzeImage/route.ts
+++ src/app/api/ai/analyzeImage/route.ts
@@ -2,6 +2,30 @@ import {NextRequest, NextResponse} from "next/server";
 import {generateObject} from 'ai';
 import {createOpenAI} from '@ai-sdk/openai';
 import {z} from 'zod';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const ticketSchema = z.object({
+    price: z.string(),
+    date: z.string(),
+    time: z.string(),
+    clinic: z.string(),
+    address: z.string(),
+    user: z.string(),
+    receipt_number: z.string(),
+    patient: z.string(),
+    hc_number: z.string(),
+    service: z.string(),
+    quantity: z.string(),
+    unit_price: z.string(),
+    total_price: z.string(),
+    amount_received: z.string(),
+    change: z.string(),
+    ticket_number: z.string(),
+    appointment_date: z.string(),
+    appointment_time: z.string()
+});
 
 const openai = createOpenAI({
     apiKey: process.env.OPENAI_API_KEY
@@ -10,32 +34,12 @@ const openai = createOpenAI({
 
 export async function POST(req: NextRequest) {
     try {
-
         const {image} = await req.json();
         const {object} = await generateObject({
             mode: 'json',
             model: openai('gpt-4o'),
             maxTokens: 4096,
-            schema: z.object({
-                price: z.string(),
-                date: z.string(),
-                time: z.string(),
-                clinic: z.string(),
-                address: z.string(),
-                user: z.string(),
-                receipt_number: z.string(),
-                patient: z.string(),
-                hc_number: z.string(),
-                service: z.string(),
-                quantity: z.string(),
-                unit_price: z.string(),
-                total_price: z.string(),
-                amount_received: z.string(),
-                change: z.string(),
-                ticket_number: z.string(),
-                appointment_date: z.string(),
-                appointment_time: z.string()
-            }),
+            schema: ticketSchema,
             messages: [
                 {
                     role: 'user',
@@ -52,7 +56,12 @@ export async function POST(req: NextRequest) {
                 }
             ]
         });
-        return NextResponse.json(object);
+
+        const ticket = await prisma.ticket.create({
+            data: object
+        });
+
+        return NextResponse.json(ticket);
     } catch (error) {
         return NextResponse.json(error);
     }
