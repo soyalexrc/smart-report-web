d017dd6a1eebfb126960f0be1bb4f05669c8d88d
alexrodriguez <alexcarvajal2404@gmail.com>
2024-06-03 17:02:41 -0500

    :tada: initial commit

---
+++ src/app/api/ai/analyzeImage/route.ts
@@ -0,0 +1,61 @@
+import {NextRequest, NextResponse} from "next/server";
+import {generateObject} from 'ai';
+import {createOpenAI} from '@ai-sdk/openai';
+import {z} from 'zod';
+
+const openai = createOpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+
+export async function POST(req: NextRequest) {
+    try {
+
+        const {image} = await req.json();
+        const {object} = await generateObject({
+            mode: 'json',
+            model: openai('gpt-4o'),
+            maxTokens: 4096,
+            schema: z.object({
+                price: z.string(),
+                date: z.string(),
+                time: z.string(),
+                clinic: z.string(),
+                address: z.string(),
+                user: z.string(),
+                receipt_number: z.string(),
+                patient: z.string(),
+                hc_number: z.string(),
+                service: z.string(),
+                quantity: z.string(),
+                unit_price: z.string(),
+                total_price: z.string(),
+                amount_received: z.string(),
+                change: z.string(),
+                ticket_number: z.string(),
+                appointment_date: z.string(),
+                appointment_time: z.string()
+            }),
+            messages: [
+                {
+                    role: 'user',
+                    content: 'Please generate an object with the data available in the following ticket.'
+                },
+                {
+                    role: 'user',
+                    content: [
+                        {
+                            type: 'image',
+                            image
+                        }
+                    ]
+                }
+            ]
+        });
+        return NextResponse.json(object);
+    } catch (error) {
+        return NextResponse.json(error);
+    }
+
+    // const stream = OpenAIStream(response);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
+import {cn} from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -10,13 +15,20 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+      <body
+          className={cn(
+              "min-h-screen bg-background font-sans antialiased",
+              fontSans.variable
+          )}
+      >
+      {children}
+      </body>
+      </html>
   );
 }
